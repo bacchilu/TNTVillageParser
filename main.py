9d6d6c0960c40b4fc38a0612499e6f189056e821
Luca Bacchi <bacchilu@bacchilu-VGN-N31Z-W.(none)>
2012-04-18 18:34:22 +0200

    Bozza di tokenizzatore 	modified:   main.py

--- main.py
+++ main.py
@@ -8,47 +8,41 @@ URL = \
     'http://www.tntvillage.scambioetico.org/index.php?act=allreleases&st=0&filter=&sb=1&sd=0&cat=4'
 
 
-class MyHTMLParser(HTMLParser.HTMLParser):
+class Tokenizer(HTMLParser.HTMLParser):
 
     def __init__(self, *args, **argd):
         HTMLParser.HTMLParser.__init__(self, *args, **argd)
-        self.count = {'table': 0, 'tr': 0}
+        self.tokens = []
 
     def handle_starttag(self, tag, attrs):
-        if tag == 'table' and ('class', 'copyright') in attrs:
-            assert self.count['table'] == 0
-            self.count['table'] = 1
-            return
-        if tag == 'table' and self.count['table'] > 0:
-            self.count['table'] += 1
-            return
-        if tag == 'tr' and self.count['table'] > 0 and self.count['tr'] \
-            == 0:
-            assert self.count['tr'] == 0
-            self.count['tr'] = 1
-            return
-        if tag == 'tr' and self.count['table'] > 0 and self.count['tr'] \
-            > 0:
-            self.count['tr'] += 1
-            return
-        if tag == 'td' and self.count['tr'] == 1:
-            print 'td'
+        self.tokens.append(('STARTTAG', tag, attrs))
 
     def handle_endtag(self, tag):
-        if tag == 'table' and self.count['table'] > 0:
-            self.count['table'] -= 1
-            return
-        if tag == 'tr' and self.count['table'] > 0:
-            assert self.count['tr'] > 0
-            self.count['tr'] -= 1
-            return
+        self.tokens.append(('ENDTAG', tag))
 
     def handle_data(self, data):
-        if self.count['tr'] == 1:
-            print data
+        self.tokens.append(('DATA', data))
+
+    def tokensGen(self):
+        return (elem for elem in self.tokens)
+
+
+def parseHeader(tokenizer):
+    t = tokenizer.next()
+    assert t[0] == 'STARTTAG'
+    assert t[1] == 'tr'
+
+
+def parseDocument(tokenizer):
+    for t in tokenizer:
+        if t[0] == 'STARTTAG' and t[1] == 'table' and ('class',
+                'copyright') in t[2]:
+            break
+    parseHeader(tokenizer)
 
 
 if __name__ == '__main__':
     response = urllib2.urlopen(URL)
-    parser = MyHTMLParser()
-    parser.feed(response.read())
+    t = Tokenizer()
+    t.feed(response.read())
+    parseDocument(t.tokensGen())
