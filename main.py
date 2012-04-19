6c5377741a052627803e8e60758b95d6c3251188
Luca Bacchi <bacchilu@bacchilu-VGN-N31Z-W.(none)>
2012-04-19 10:30:10 +0200

    Refactoring 	modified:   main.py 	renamed:    main.py -> tntparser.py

--- main.py
+++ main.py
@@ -1,51 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import urllib2
-from lxml import etree
-
-URL = \
-    'http://www.tntvillage.scambioetico.org/index.php?act=allreleases&st=%d&filter=&sb=1&sd=0&cat=4'
-
-
-def getUrl(page):
-    return URL % (page * 35)
-
-
-def getPageContent(page):
-    response = urllib2.urlopen(getUrl(page))
-    parser = etree.HTMLParser()
-    tree = etree.parse(response, parser)
-    root = tree.getroot()
-    tableElement = \
-        root.findall('body/div/div/table/tr/td/div/table/tr/td/div/table[@class="copyright"]'
-                     )[0]
-    rows = tableElement.findall('tr')
-    rows = [e for (i, e) in enumerate(rows) if i > 0]
-    values = []
-    for r in rows:
-        d = {'title': r[1].findall('a')[0].text,
-             'torrent': r[0].findall('span/a')[1].attrib['href']}
-        values.append(d)
-    return values
-
-
-def pagesGenerator():
-    page = 0
-    while True:
-        ret = getPageContent(page)
-        if len(ret) == 0:
-            return
-        yield ret
-        page += 1
-
-
-def getTorrents():
-    for page in pagesGenerator():
-        for t in page:
-            yield t
-
+import tntparser
 
 if __name__ == '__main__':
-    for (i, film) in enumerate(getTorrents()):
+    for (i, film) in enumerate(tntparser.filmGenerator()):
         print i, film['title']
